0798356836821e7c49ea6d4416d9624f27489277
SToyokazu <syoubousya945@yahoo.co.jp>
2022-07-19 00:22:33 +0900

    GC対応

--- code/CookiePlayerFunction.js
+++ code/CookiePlayerFunction.js
@@ -29,3 +29,24 @@ function AutoCookieClickFunction(e)
     }
     
 }
+
+function AutoGCClickFunction(e)
+{
+    arguments.callee.id = 0;
+
+    var AllGCPop = function()
+    {
+        for (var i in Game.shimmers) {
+             Game.shimmers[i].pop(); 
+        }
+    }
+
+    if(e.data.status ===  true){
+        this.id = setInterval(AllGCPop,500);
+        console.log("Auto GC click on: "+ this.id);
+    }else{
+        console.log("Auto GC click off: "+this.id);
+        clearInterval(this.id);
+    }
+    
+}
--- code/CookiePlayerMessage.js
+++ code/CookiePlayerMessage.js
@@ -10,33 +10,3 @@ function AutoMessage(modeName)
 
     window.opener.postMessage({ message: modeName, status: modeStatus }, '*');
 }
-
-/*
-function AutoCookieClickMessage()
-{
-    var modeMessage = "AutoCookieClick";
-    var modeStatus = true;
-
-    if(document.getElementById('AutoCookieClick').checked ===  true){
-        modeStatus = true;
-    }else{
-        modeStatus = false;
-    }
-
-    window.opener.postMessage({ message: modeMessage, status: modeStatus }, '*');
-}
-
-function AutoGCClickMessage()
-{
-    var modeMessage = "AutoGCClick";
-    var modeStatus = true;
-
-    if(document.getElementById('AutoGCClick').checked ===  true){
-        modeStatus = true;
-    }else{
-        modeStatus = false;
-    }
-
-    window.opener.postMessage({ message: modeMessage, status: modeStatus }, '*');
-}
-*/
