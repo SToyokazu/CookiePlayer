364ce1b7bc1590bf61eaab6d632172d003e156fc
SToyokazu <syoubousya945@yahoo.co.jp>
2022-07-18 23:54:38 +0900

    メッセージの送信関数の設計変更

--- code/CookiePlayerMessage.js
+++ code/CookiePlayerMessage.js
@@ -1,3 +1,17 @@
+function AutoMessage(modeName)
+{
+    var modeStatus = true;
+
+    if(document.getElementById(modeName).checked ===  true){
+        modeStatus = true;
+    }else{
+        modeStatus = false;
+    }
+
+    window.opener.postMessage({ message: modeName, status: modeStatus }, '*');
+}
+
+/*
 function AutoCookieClickMessage()
 {
     var modeMessage = "AutoCookieClick";
@@ -11,3 +25,18 @@ function AutoCookieClickMessage()
 
     window.opener.postMessage({ message: modeMessage, status: modeStatus }, '*');
 }
+
+function AutoGCClickMessage()
+{
+    var modeMessage = "AutoGCClick";
+    var modeStatus = true;
+
+    if(document.getElementById('AutoGCClick').checked ===  true){
+        modeStatus = true;
+    }else{
+        modeStatus = false;
+    }
+
+    window.opener.postMessage({ message: modeMessage, status: modeStatus }, '*');
+}
+*/
