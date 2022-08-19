9194c4bbf0181e6190743299ca86aba639eb1dbc
SToyokazu <syoubousya945@yahoo.co.jp>
2022-08-19 23:56:52 +0900

    loadするスクリプトを追加

--- code/CookiePlayer.js
+++ code/CookiePlayer.js
@@ -1,2 +1,3 @@
 window.open("https://stoyokazu.github.io/CookiePlayer/code/index.html", "", "width=350,height=350,scrollbars=yes,resizable=yes,status=yes");
 Game.LoadMod("https://stoyokazu.github.io/CookiePlayer/code/CookiePlayerFunction.js");
+Game.LoadMod("https://stoyokazu.github.io/CookiePlayer/code/CookieClickerSimulate.js");
